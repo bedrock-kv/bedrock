781f8d6d04a25933f3d4fa453a32d5dc5f599110
Jason Allum <21417+jallum@users.noreply.github.com>
2024-09-16 22:07:05 -0400

    Added doc; conveniences.

--- lib/bedrock/control_plane/config.ex
+++ lib/bedrock/control_plane/config.ex
@@ -1,9 +1,14 @@
 defmodule Bedrock.ControlPlane.Config do
+  @moduledoc """
+  This module defines the configuration structure and functions for the Bedrock
+  Control Plane. It provides functions for accessing and manipulating the
+  configuration parameters, policies and transaction system layout.
+  """
   alias Bedrock.ControlPlane.Config.Policies
   alias Bedrock.ControlPlane.Config.TransactionSystemLayout
 
   @type t :: %__MODULE__{
-          state: atom(),
+          state: state(),
           parameters: map(),
           policies: Policies.t(),
           transaction_system_layout: TransactionSystemLayout.t()
@@ -22,29 +27,66 @@ defmodule Bedrock.ControlPlane.Config do
     transaction_system_layout: nil
   ]
 
-  @spec valid_states() :: [atom()]
-  def valid_states, do: ~w[
-    initializing
-  ]a
+  @type state :: :initializing | :running | :stopping
 
+  @doc """
+  Creates a new configuration struct.
+  """
+  @spec new(state(), parameters :: map(), Policies.t(), TransactionSystemLayout.t()) :: t()
+  def new(state, parameters, policies, transaction_system_layout) do
+    %__MODULE__{
+      state: state,
+      parameters: parameters,
+      policies: policies,
+      transaction_system_layout: transaction_system_layout
+    }
+  end
+
+  @doc """
+  Returns true if the cluster will allow volunteer nodes to join.
+  """
   @spec allow_volunteer_nodes_to_join?(t()) :: boolean()
   def allow_volunteer_nodes_to_join?(t), do: t.policies.allow_volunteer_nodes_to_join
 
+  @doc """
+  Returns the nodes that are part of the cluster.
+  """
   @spec nodes(t()) :: [node()]
-  def nodes(t), do: t.parameters.nodes
+  def nodes(t), do: t.parameters[:nodes] || []
 
+  @doc """
+  Returns the ping rate in milliseconds.
+  """
   @spec ping_rate_in_ms(t()) :: non_neg_integer()
   def ping_rate_in_ms(t), do: div(1000, t.parameters.ping_rate_in_hz)
 
-  @spec sequencer(t()) :: atom() | nil
+  @doc """
+  Returns the pid of the sequencer.
+  """
+  @spec sequencer(t()) :: pid() | nil
   def sequencer(t), do: find_singleton_service(t, :sequencer)
 
-  @spec data_distributor(t()) :: atom() | nil
+  @doc """
+  Returns the pid of the data distributor.
+  """
+  @spec data_distributor(t()) :: pid() | nil
   def data_distributor(t), do: find_singleton_service(t, :data_distributor)
 
+  @doc """
+  Returns the otp_names of the log workers.
+  """
   @spec log_workers(t()) :: [atom()]
   def log_workers(t), do: find_multiple_services(t, :log)
 
+  @doc """
+  Returns the otp_names of the storage workers.
+  """
+  @spec storage_workers(t()) :: [atom()]
+  def storage_workers(t), do: find_multiple_services(t, :storage)
+
+  @doc """
+  Returns the entire service directory.
+  """
   def service_directory(%__MODULE__{
         transaction_system_layout: %TransactionSystemLayout{
           service_directory: service_directory

960e0d434515f13bf89faa6a97f12ed15085a1f8
Jason Allum <21417+jallum@users.noreply.github.com>
2024-09-17 19:42:16 -0400

    Refactoring.

--- lib/bedrock/control_plane/config/policies.ex
+++ lib/bedrock/control_plane/config/policies.ex
@@ -1,10 +1,17 @@
 defmodule Bedrock.ControlPlane.Config.Policies do
-  defstruct [
-    # Should nodes that volunteer to join the cluster be allowed to do so?
-    allow_volunteer_nodes_to_join: true
-  ]
+  @moduledoc """
+  A `Policies` is a data structure that describes the policies that are used
+  """
 
+  @typedoc """
+  Struct representing the policies that are used to configure the cluster.
+
+  ## Fields:
+  - `allow_volunteer_nodes_to_join` - Should nodes that volunteer to join the cluster be allowed to do so?
+  """
   @type t :: %__MODULE__{
           allow_volunteer_nodes_to_join: boolean()
         }
+
+  defstruct allow_volunteer_nodes_to_join: true
 end

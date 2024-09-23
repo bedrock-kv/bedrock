e42a25ef4bd3bd57347e2362570795698cc8d1ad
Jason Allum <21417+jallum@users.noreply.github.com>
2024-09-23 17:28:24 -0400

    Made it so the log worker can be started.

--- lib/bedrock/service/transaction_log_worker/limestone.ex
+++ lib/bedrock/service/transaction_log_worker/limestone.ex
@@ -1,11 +1,42 @@
 defmodule Bedrock.Service.TransactionLogWorker.Limestone do
-  use Bedrock.Service.WorkerBehaviour
   use Supervisor
+  use Bedrock.Service.WorkerBehaviour
 
-  alias Bedrock.Service.Controller
   alias Bedrock.Service.TransactionLogWorker.Limestone.SegmentRecycler
   alias Bedrock.Service.TransactionLogWorker.Limestone.Transactions
   alias Bedrock.Service.TransactionLogWorker.Limestone.TransactionReceiver
+
+  defmodule Server do
+    use GenServer
+
+    def child_spec(opts) do
+      otp_name = opts[:otp_name] || raise "Missing :otp_name option"
+
+      %{
+        id: __MODULE__,
+        start:
+          {GenServer, :start_link,
+           [
+             __MODULE__,
+             opts,
+             [name: otp_name]
+           ]},
+        restart: :permanent
+      }
+    end
+
+    @impl GenServer
+    def init(opts) do
+      IO.inspect(opts)
+      {:ok, :nostate}
+    end
+
+    @impl GenServer
+    def handle_call({:info, _fact_names}, _from, state) do
+      IO.inspect("asdasdasd!")
+      {:reply, {:ok, []}, state}
+    end
+  end
 
   def child_spec(opts) do
     otp_name = opts[:otp_name] || raise "Missing :otp_name option"
@@ -14,27 +45,25 @@ defmodule Bedrock.Service.TransactionLogWorker.Limestone do
     controller = Keyword.fetch!(opts, :controller)
 
     %{
-      id: __MODULE__,
+      id: {__MODULE__, id},
       start:
         {Supervisor, :start_link,
          [
            __MODULE__,
            {
              otp_name,
-             id,
              path,
              opts[:minimum_available] || 3,
              opts[:segment_size] || 64 * 1024 * 1024,
              controller
-           },
-           [name: otp_name]
+           }
          ]},
       type: :supervisor
     }
   end
 
   @impl Supervisor
-  def init({otp_name, id, path, minimum_available, segment_size, controller}) do
+  def init({otp_name, path, minimum_available, segment_size, controller}) do
     transactions = Transactions.new(:"#{otp_name}_transactions")
 
     recycler_name = :"#{otp_name}_recycler"
@@ -55,10 +84,14 @@ defmodule Bedrock.Service.TransactionLogWorker.Limestone do
            recycler: recycler_name,
            controller: controller,
            otp_name: transaction_receiver_name
+         ]},
+        {Server,
+         [
+           otp_name: otp_name
          ]}
       ]
 
-    Controller.report_worker_health(controller, id, :ok)
+    # Controller.report_worker_health(controller, id, :ok)
 
     Supervisor.init(children, strategy: :one_for_one)
   end

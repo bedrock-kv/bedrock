b36277dbdb8b8d7dbf1821861aaa68815c3e33d9
Jason Allum <21417+jallum@users.noreply.github.com>
2024-10-03 23:03:31 -0400

    Added doc; specs

--- lib/bedrock.ex
+++ lib/bedrock.ex
@@ -5,5 +5,7 @@ defmodule Bedrock do
   @type key_range :: {min_inclusive :: key(), max_exclusive :: key() | nil}
   @type epoch :: non_neg_integer()
   @type timeout_in_ms :: :infinity | non_neg_integer()
+
   @type service :: :coordination | :log | :storage
+  @type service_id :: String.t()
 end
--- lib/bedrock/control_plane/config/service_descriptor.ex
+++ lib/bedrock/control_plane/config/service_descriptor.ex
@@ -1,5 +1,23 @@
 defmodule Bedrock.ControlPlane.Config.ServiceDescriptor do
-  @type id :: String.t()
+  @moduledoc """
+  A module representing a service descriptor within the Bedrock control plane configuration.
+
+  This module provides a structure for defining a service descriptor, which includes an `id`,
+  `type`, and `pid`. It is used to describe and instantiate service components within the
+  Bedrock system.
+
+  ## Types
+
+    * `id` - Represents the unique identifier of the service, corresponding to a `String.t()`.
+    * `type` - Represents the type of the service, corresponding to a `Bedrock.service()`.
+    * `t` - A struct of the service descriptor containing `id`, `type`, and `pid`.
+
+  ## Functions
+
+    * `new/3` - Creates a new service descriptor with given `id`, `type`, and `pid`.
+  """
+
+  @type id :: Bedrock.service_id()
   @type type :: Bedrock.service()
 
   @type t :: %__MODULE__{
@@ -11,6 +29,9 @@ defmodule Bedrock.ControlPlane.Config.ServiceDescriptor do
             type: nil,
             pid: nil
 
+  @doc """
+  Creates a new service descriptor.
+  """
   @spec new(id(), type(), pid()) :: t()
   def new(id, type, pid) do
     %__MODULE__{

a5d5d55e56283554d7353ec840409ded7dee4fce
Jason Allum <21417+jallum@users.noreply.github.com>
2024-10-24 18:32:00 -0400

    wip

---
+++ lib/bedrock/internal/logging/coordination_telemetry.ex
@@ -0,0 +1,30 @@
+defmodule Bedrock.Internal.Logging.CoordinationTelemetry do
+  require Logger
+
+  defp handler_id, do: "bedrock_logging_coordination_telemetry"
+
+  def start do
+    :telemetry.attach_many(
+      handler_id(),
+      [
+        [:bedrock, :cluster, :coordination, :consensus_reached]
+      ],
+      &__MODULE__.log_event/4,
+      nil
+    )
+  end
+
+  def stop, do: :telemetry.detach(handler_id())
+
+  def log_event(
+        [:bedrock, :cluster, :coordination, :consensus_reached],
+        _measurements,
+        %{cluster: cluster, transaction_id: transaction_id} = _metadata,
+        _config
+      ) do
+    Logger.info(
+      "Bedrock [#{cluster.name()}]: Reached consensus at transaction #{inspect(transaction_id)}.",
+      ansi_color: :green
+    )
+  end
+end

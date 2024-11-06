5ee87f8766b9a257cc686ae094b8abdcd6ce19ab
Jason Allum <21417+jallum@users.noreply.github.com>
2024-11-05 22:23:11 -0500

    Cleanup

--- lib/bedrock/data_plane/sequencer/controller_feedback.ex
+++ lib/bedrock/data_plane/sequencer/controller_feedback.ex
@@ -1,23 +1,17 @@
 defmodule Bedrock.DataPlane.Sequencer.DirectorFeedback do
   alias Bedrock.DataPlane.Sequencer.State
 
+  import Bedrock.Internal.GenServer.Calls
+
   @spec accept_invitation(State.t()) :: State.t()
   def accept_invitation(t) do
-    GenServer.cast(
-      t.director,
-      {:recruitment_invitation, :accepted, self(), t.last_committed_version}
-    )
-
+    cast(t.director, {:recruitment_invitation, :accepted, self(), t.last_committed_version})
     t
   end
 
   @spec decline_invitation(State.t(), director :: pid()) :: State.t()
   def decline_invitation(t, director) do
-    GenServer.cast(
-      director,
-      {:recruitment_invitation, :declined, self()}
-    )
-
+    cast(director, {:recruitment_invitation, :declined, self()})
     t
   end
 end

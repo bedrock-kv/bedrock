198938cce3c2f20e034c913b70eb3c2113384a48
Jason Allum <21417+jallum@users.noreply.github.com>
2026-09-10 08:12:59 -0400

    bedrock-puz: clean up registered routing stubs

--- test/bedrock/internal/repo_transact_test.exs
+++ test/bedrock/internal/repo_transact_test.exs
@@ -426,7 +426,21 @@ defmodule Bedrock.Internal.RepoTransactTest do
     defp spawn_stub_materializer(test_pid, answers \\ 1) do
       materializer = spawn(fn -> materializer_loop(test_pid, answers) end)
       Process.register(materializer, RoutingCluster.otp_name_for_worker("wkr1"))
+      stop_on_exit(materializer)
       materializer
+    end
+
+    defp stop_on_exit(pid) do
+      on_exit(fn ->
+        ref = Process.monitor(pid)
+        Process.exit(pid, :kill)
+
+        receive do
+          {:DOWN, ^ref, :process, ^pid, _reason} -> :ok
+        after
+          1_000 -> raise "registered materializer did not stop"
+        end
+      end)
     end
 
     defp materializer_loop(_test_pid, 0), do: :ok
